57a735b6270f7a153d1d830df347f14bd0218b9b
蔡金锋 <jeff.cai@99bill.com>
2020-03-18 17:28:07 +0800

    修改默认配置

--- doly.config.js
+++ doly.config.js
@@ -1,19 +1,19 @@
 const path = require("path");
+const pkg = require("./package.json");
 
 module.exports = {
   html: {
     template: "src/document.ejs",
-    filename: "index.html",
-    minify: {
-      removeComments: true,
-      collapseWhitespace: true
-    }
+    filename: "index.html"
   },
 
   // 别名
   alias: {
     "~": path.join(__dirname, "./src")
   },
+
+  // 构建完成后生成zip包路径
+  zip: `_build/${pkg.name}-${pkg.version}.zip`,
 
   // 扩展 babel-loader 的 plugins
   extraBabelPlugins: [
@@ -28,11 +28,10 @@ module.exports = {
       "@wonder-ui/core"
     ]
   ],
-  cssInline: true,
 
   // 定义编译时变量替换
   define: {
-    // api 地址
+    // 开发环境的api地址
     API_URL: "",
     DEV: true
   },
@@ -41,9 +40,27 @@ module.exports = {
   env: {
     // 生产环境
     production: {
-      publicPath: "./",
+      publicPath: "./", // 静态资源域名，如 https://img.99bill.com/
+      html: {
+        template: "src/document.ejs",
+        filename: "seashell/webapp/product/project/index.html", // 构建后的html路径，如 seashell/webapp/x-project/agent/index.html
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true
+        }
+      },
+      outputFilename: "res/j/[name].[chunkhash:8].js", // js入口构建后路径
+      outputChunkFilename: "res/j/[name].[chunkhash:8].chunk.js", // js块构建后路径
+      css: {
+        filename: "res/c/[name].[contenthash:8].css", // css文件构建后路径
+        chunkFilename: "res/c/[name].[contenthash:8].chunk.css" // css块构建后路径
+      },
+      image: {
+        name: "res/i/[name].[hash:8].[ext]", // 图片构建后路径
+        limit: 1024 * 8 // 小于该大小的图片转为base64
+      },
       define: {
-        API_URL: ""
+        API_URL: "" // 生产环境的api地址
       }
     }
   }
